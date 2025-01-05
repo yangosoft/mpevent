bc325086e1d4da21fa6f720913aed8a15bff996c
Yangosoft <yangosoft@gmail.com>
2025-01-05 01:18:46 +0100

    initial commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,3 @@
-
-pub mod subscriber;
-pub mod event;
 pub mod coordinator;
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub mod event;
+pub mod subscriber;
